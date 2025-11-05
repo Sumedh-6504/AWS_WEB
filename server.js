ca2c6598222d32825c47ee2cb4ee74058501b23c
Sumedh-R-Mundewadi <sumedhrm.gdsc@gmail.com>
2025-11-05 10:09:16 +0530

    Added My name

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.get("/", (req, res) => res.send("Hello from AWS EC2 Web Server!"));
+app.get("/", (req, res) => res.send("Hello from AWS EC2 Web Server!--Sumedh_AWS_Migration"));
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
